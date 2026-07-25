431680d6469598eb181c14668780b9a005661890
FARIYA🩵 <fariya.github@gmail.com>
2026-07-25 12:41:10 +0600

    Add problem description for Domino piling 50A
    Added problem description and input/output details for the Domino piling problem.

--- domino_piling_50A.py
+++ domino_piling_50A.py
@@ -1,3 +1,15 @@
+"""
+Problem Name: Domino piling
+Platform: Codeforces (50A)
+Problem Link: https://codeforces.com/problemset/problem/50/A
+
+Description:
+M x N সাইজের একটি চারকোনা বোর্ডে ২ x ১ সাইজের ডমিনো বসাতে হবে। 
+বোর্ডে সর্বোচ্চ কতটি ডমিনো বসানো সম্ভব?
+
+ইনপুট: দুটি ইন্টিজার M এবং N।
+আউটপুট: সর্বোচ্চ ডমিনোর সংখ্যা ((M * N) // 2)।
+"""
 m,n=map(int,input().split())
 
 ghor=m*n
