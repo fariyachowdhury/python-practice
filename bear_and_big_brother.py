1c7640651ab204adcb8557ae8ae4a65ebca0b08c
FARIYA🩵 <fariya.github@gmail.com>
2026-07-25 12:38:33 +0600

    Add solution for Bear and Big Brother problem
    Implement solution for 'Bear and Big Brother' problem.

--- bear_and_big_brother.py
+++ bear_and_big_brother.py
@@ -1,3 +1,17 @@
+"""
+Problem Name: Bear and Big Brother
+Platform: Codeforces (791A)
+Problem Link: https://codeforces.com/problemset/problem/791/A
+
+Description:
+ভালুক লিমাক (ওজন a) আর তার বড় ভাই বব (ওজন b)। লিমাকের ওজন প্রতি বছর ৩ গুণ হয় (a * 3) 
+এবং ববের ওজন প্রতি বছর ২ গুণ হয় (b * 2)। 
+কয় বছর পর লিমাকের ওজন ববের ওজনের চেয়ে কঠোরভাবে বেশি (a > b) হবে?
+
+ইনপুট: দুটি ইন্টিজার a এবং b।
+আউটপুট: মোট কত বছর লাগবে সেই সংখ্যা।
+"""
+#সমাধান
 a,b=map(int,input().split())
 year=0
 while a<=b:
